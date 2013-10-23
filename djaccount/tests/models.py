e195310ab7535929e5e3c9593eba90f7915f0312
Lucas Tan <tbhtan3@gmail.com>
2013-10-23 22:50:54 +0800

    added more tests

--- djaccount/tests/models.py
+++ djaccount/tests/models.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.test.client import RequestFactory, Client
 from django.utils import timezone
+
+from djbase.utils import mock
 
 from djaccount.models import Account
 from djaccount.exceptions import *
@@ -12,9 +15,23 @@ class AccountManagerTest(TestCase):
     FIRST_NAME  = 'John'
     PASSWORD    = '123'
 
+    def setUp(self):
+        self.client = Client()
+        self.factory = RequestFactory()
+
     def _create(self):
         acc = Account.objects.create_user(email=self.EMAIL, first_name=self.FIRST_NAME, password=self.PASSWORD)
         return acc 
+
+    def test_case_sensitive_email(self):
+        acc = Account.objects.create_user(email=self.EMAIL.lower(), first_name=self.FIRST_NAME, password=self.PASSWORD)
+        
+        try:
+            acc2 = Account.objects.get(email=self.EMAIL.upper())
+        except Account.DoesNotExist:
+            print 'WARNING: Email is case sensitive'
+        finally:
+            acc.delete()
 
     def test_create_user(self):
         acc = self._create()
@@ -116,3 +133,63 @@ class AccountManagerTest(TestCase):
         finally:
             acc.delete()
 
+    def test_login(self):
+        acc = self._create()
+
+        req = self.factory.get('/')
+        req.session = mock.Session()
+
+        try:
+            acc2 = Account.objects.login(email=acc.email, password=self.PASSWORD, request=req)
+            self.assertTrue(acc2 is not None)
+            self.assertEqual(acc2.pk, acc.pk)
+
+            #req.user is not set
+            #self.assertTrue(req.user)            
+            #self.assertTrue(req.user.is_authenticated())
+            #self.assertTrue(req.user.id, acc.id)
+            #self.assertTrue(req.user.id, acc2.id)
+
+            with self.assertRaises(AccountLoginError):
+                Account.objects.login(email=acc.email, password=self.PASSWORD+'0', request=req)
+        
+            with self.assertRaises(AccountMissingError):
+                Account.objects.login(email=acc.email+'0', password=self.PASSWORD, request=req)
+        finally:
+            acc.delete()
+
+    def _test_password_reset(self, acc):
+        token = Account.objects.create_password_reset(account=acc)
+        self.assertTrue(len(token) > 0)
+        
+        token2 = Account.objects.create_password_reset(account=acc)
+        self.assertEqual(token2, None)
+        
+        req = Account.objects.do_password_reset(token=token, new_password=self.PASSWORD*2)
+        pwd_valid = req.account.check_password(self.PASSWORD*2)
+        self.assertTrue(pwd_valid)
+        self.assertTrue(req.is_done)
+        self.assertTrue(req.changed >= req.created)
+
+        # check that it doesn't allow creating within threshold
+        token2 = Account.objects.create_password_reset(account=acc)
+        self.assertEqual(token2, None)
+        
+        # check whether new password is saved
+        acc = Account.objects.get(pk=acc.pk)
+        pwd_valid = acc.check_password(self.PASSWORD*2)
+        self.assertTrue(pwd_valid)
+        
+        with self.assertRaises(AccountPasswordResetExpiredError): 
+            Account.objects.do_password_reset(token=token, new_password=self.PASSWORD*2)
+
+        with self.assertRaises(AccountPasswordResetMissingError): 
+            Account.objects.do_password_reset(token=token+'0', new_password=self.PASSWORD*2)
+
+    def test_create_password_reset(self):
+        acc = self._create()
+        
+        try:
+            self._test_password_reset(acc)
+        finally:
+            acc.delete()
