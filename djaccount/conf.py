1aecdc4e7aef24969784a1cb9b28e53fb1c45332
Lucas Tan <tbhtan3@gmail.com>
2013-10-23 22:24:06 +0800

    added djaccount config

---
+++ djaccount/conf.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+
+import hashlib
+
+from django.conf import settings as django_settings
+
+def _hash(s):
+    m = hashlib.sha1()
+    m.update(s)
+    return m.hexdigest().lower()
+
+class _Settings(object):
+    DEFAULTS = {
+        'ACCOUNT_PASSWORD_RESET_EXPIRY_MINUTES'     :   120,
+        'ACCOUNT_PASSWORD_RESET_THRESHOLD_MINUTES'  :   10,
+        'ACCOUNT_SECRET_KEY'                        :   _hash(django_settings.SECRET_KEY)
+    }
+
+    def __getattr__(self, name):
+        def_val = self.DEFAULTS.get(name, None)
+
+        if def_val is not None:
+            return getattr(django_settings, name, def_val)
+
+        return def_val
+
+settings = _Settings()
