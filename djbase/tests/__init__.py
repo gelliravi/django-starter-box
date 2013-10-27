c00257a8a55eab4ae707e76d289cceb32e4ff076
Lucas Tan <tbhtan3@gmail.com>
2013-10-27 16:11:20 +0800

    cleaned up and added tests for FixedCharField

--- djbase/tests/__init__.py
+++ djbase/tests/__init__.py
@@ -1,12 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models 
-from djbase.models import PickleField
+from djbase.models import PickleField, FixedCharField
 
-class PickleModelTest(models.Model):
-    p = PickleField()
-    p2 = PickleField(default=(1,2))
+# Must put models here so they can be synced during test.
 
+class PickleFieldTestModel(models.Model):
+    optional = PickleField(null=True, blank=True, default=(1,2))
+    required = PickleField(null=False, blank=False) # default is None
+    
+class FixedCharFieldTestModel(models.Model):
+    optional = FixedCharField(null=True, blank=True, max_length=10, default='a')
+    required = FixedCharField(null=False, blank=False, max_length=20)
 
 from .models import *
 from .utils import *
--- djbase/tests/models.py
+++ djbase/tests/models.py
@@ -2,9 +2,9 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 from django.db import models
-from djbase.models import PickleField
+from djbase.models import PickleField, FixedCharField
 
-from . import PickleModelTest
+from . import PickleFieldTestModel, FixedCharFieldTestModel
 
 class CustomType(list):
     test = []
@@ -12,19 +12,66 @@ class CustomType(list):
 class PickleFieldTest(TestCase):
     def test(self):
         test_data = (
-            None, 123, (1,2), [], {}, {1:2, 3:4}, {'1': 1},
-            CustomType()
+            123, (), (None,), (1,2), [], [1,2], {}, {1:2, 3:4}, {'1': 1},
+            CustomType(), 
         )
 
         test_data2 = [e for e in test_data]
         test_data2.append(test_data)
 
         for input in test_data2:
-            m = PickleModelTest(p=input)
-            self.assertEqual(m.p2, (1,2))
+            m = PickleFieldTestModel(required=input)
+            self.assertEqual(m.optional, (1,2))
+            self.assertEqual(m.required, input)
+
+            m.clean()
             m.save()
+            self.assertEqual(m.optional, (1,2))
+            self.assertEqual(m.required, input)
 
-            m2 = PickleModelTest.objects.get(pk=m.pk)
-            self.assertEqual(m.p, m2.p)
-            self.assertEqual(m2.p2, (1,2))
+            m2 = PickleFieldTestModel.objects.get(pk=m.pk)
+            self.assertEqual(m2.required, m.required)
+            self.assertEqual(m2.optional, m.optional)
 
+        test_data2.append(None)
+
+        for input in test_data2:
+            m3 = PickleFieldTestModel(required={}, optional=input)
+            self.assertEqual(m3.optional, input)
+            self.assertEqual(m3.required, {})
+
+            m3.clean()
+            m3.save()
+            self.assertEqual(m3.optional, input)
+            self.assertEqual(m3.required, {})
+
+            m4 = PickleFieldTestModel.objects.get(pk=m3.pk)
+            self.assertEqual(m4.optional, input)
+            self.assertEqual(m4.required, {})
+
+class FixedCharFieldTest(TestCase):
+    def test(self):
+        test_data = (
+            ('', ''),
+            ('   ', ''),
+            (' a ','a'),
+            (b'  ', ''),
+            # (None, None), # Cannot use None as it is treated as Null
+        )
+
+        for pairs in test_data:
+            input = pairs[0]
+            expected = pairs[1]
+
+            m = FixedCharFieldTestModel(required=input)
+            self.assertEqual(m.required, expected)
+            self.assertEqual(m.optional, 'a')
+
+            m.clean()
+            m.save()
+            self.assertEqual(m.required, expected)
+            self.assertEqual(m.optional, 'a')
+
+            m2 = FixedCharFieldTestModel.objects.get(pk=m.pk)
+            self.assertEqual(m2.required, m.required)
+            self.assertEqual(m2.optional, m.optional)
