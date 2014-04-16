1bc5e05ea1523b25878216f8db6b3e3b8b35be84
Lucas Tan <tbhtan3@gmail.com>
2014-04-16 21:07:51 +0800

    increased version to 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-starter-box',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='Django starter box',
     long_description='Starter box',
